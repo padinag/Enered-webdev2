a0b59189dad308b5b8da8e6c98eeac13f0bdb745
adina <adina@mini-box.com>
2020-01-15 17:42:02 +0200

    tema27

--- ballAnimation.js
+++ ballAnimation.js
@@ -10,13 +10,18 @@ class BallAnimation {
     }
 
     addBallProperties() {
-        this.element.display = "inline-block"
-        this.element.style.width = `${this.size}px`;
-        this.element.style.height = `${this.size}px`;
-        this.element.style.background = "green";
-        this.element.position = "absolute";
-        this.element.borderRadius = 50
-    }
+        this.element.style.display = "inline-block",
+        this.element.style.width = `${this.size}px`,
+        this.element.style.height = `${this.size}px`,
+        this.element.style.background = "green",
+        this.element.style.position = "absolute",
+        this.element.style.borderRadius = "50%",
+        this.element.style.borderStyle = "solid",
+        this.element.style.borderSize = "0px",
+        this.element.style.borderColor = "green",
+       this.element.style.top = `${this.startY}px`,
+        this.element.style.left = `${this.startX}px`
+       }
 
     startAnimation(){
         this.anim = requestAnimationFrame(this.animate);
@@ -27,9 +32,33 @@ class BallAnimation {
     }
 
     animate() {
-        
-        
-        
+        var horizontal = true;
+        var pas = 1;
+        var repeatNo = 5;
+        var leftLimit = 50;
+        var topLimit = 50;
+
+         if(horizontal){
+        for (var i = 0; i < repeatNo; i++){
+        startX += pas;
+        if (startX > endX || startX < leftLimit) {
+            console.log(startX);
+            pas *= -1; 
+        }
+        this.element.style.left = `${startX}px`;
+        // horizontal = false;
+    }
+         } else{
+            for (var i = 0; i < repeatNo; i++){
+                startY -= pas;
+                if (startY < endY || startY < topLimit) {
+                    console.log(startY);
+                    pas *= -1; 
+                }
+         }
+        this.element.style.top = `${startY}px`;
+
+        } 
         
         this.anim = requestAnimationFrame(this.animate);
     }
