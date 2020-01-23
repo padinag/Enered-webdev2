f106feb55543f53cccb132cda0ff4d8fcfd17b4c
adina <adina@mini-box.com>
2020-01-23 20:16:27 +0200

    classes

--- curs29/beeing.js
+++ curs29/beeing.js
@@ -1,17 +1,29 @@
 class Beeing {
     constructor(options){
+        this.name = options.name;
         this.alive = options.alive;
         this.mamifer = options.mamifer;
         this.age = options.age;
-        this.get = options.gen;
+        this.gen = options.gen;
         this.geographicalRegion = options.geographicalRegion;
     }
 
     isOld(){
-        if (age > 60){
-            return true;
+        if (this.age > 60){
+            // return true;
+            console.log(true);
         } else {
-            return false
+            // return false;
+            console.log(false);
         }
+    }
+    breathe(){
+        // return (this.name + "breathe!");
+        console.log(this.name + " " + "breathe!");
+    }
+    
+    eat(){
+        // return (this.name + "eats!")
+        console.log(this.name + " " + "eats!");
     }
 }
--- curs29/human.js
+++ curs29/human.js
@@ -1,8 +1,15 @@
-class Human extends Beeing{
-    constructor(options){
-        options.mamifer = true; //modific proprietati - suprascriu
+class Human extends Beeing {
+    constructor(options) {
         super(options);
+        options.mamifer = true; //modific proprietati - suprascriu
         this.married = options.merried;
+        this.address = options.address;
+        this.phoneNumber = options.phoneNumber;
+        this.emailAddress = options.emailAddress;
+    }
+
+    isVegan(){
+        console.log(this.name + " este carnivora!");
     }
 }
 
