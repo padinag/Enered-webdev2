91b9bd11f65e38785163f12497d26bec113e9284
adina <adina@mini-box.com>
2020-01-29 16:48:01 +0200

    classes-sprite-tema

--- curs30/arrowSprite.js
+++ curs30/arrowSprite.js
@@ -2,19 +2,42 @@ class ArrowSprite extends Sprite {
   constructor(options) {
     options.class = options.classes[0];
     super(options);
-    this._classes = options.clasess;
+    this._classes = options.classes;
     this._index = 0;
-    this.pressKeyCallback = options.pressKeyCallback;
-    this.init();
+    // this.pressKeyCallback = options.pressKeyCallback;
+    this.initialization();
   }
 
-  init() {
+  initialization() {
     console.log("press any key");
-    this._element.addEventListener("onkeydown", this.pressKey.bind(this));
+    console.log(this._element);
+    this._element.tabIndex = 0;
+    this._element.addEventListener("keydown", this.pressKey.bind(this));
   }
 
   pressKey(e) {
-    console.log.bind("pressedKey");
-    this.pressKeyCallback(e);
+    console.log(e);
+    console.log(this._classes);
+
+    // this.pressKeyCallback(e);
+    console.log("keypressed");
+    let x = e.code;
+    console.log(x);
+    if (x == "ArrowLeft" || x == "ArrowDown") {
+      this._index--;
+      if (this._index <= 0) {
+        this._index = this._classes.length-1;
+      }
+      console.log(this._index);
+      this.class = this._classes[this._index];
+    } else if (x == "ArrowRight" || x == "ArrowUp") {
+      this._index++;
+      if (this._index >= this._classes.length) {
+        this._index = 0;
+      }
+      this.class = this._classes[this._index];
+    } else {
+      alert("press an arrow key");
+    }
   }
 }
